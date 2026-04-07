95a0166daa9ffbeb138adb66c913d520b55a2120
UihwanLee <luh5063014@naver.com>
2026-04-07 16:18:55 +0900

    feat: Button OnClick시 애니메이션 구현

--- Assets/02.Scripts/UIController.cs
+++ Assets/02.Scripts/UIController.cs
@@ -2,6 +2,7 @@ using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
@@ -12,10 +13,18 @@ public class UIController : MonoBehaviour
     public CanvasGroup canvasGroup;
     public float duration;
 
+    public Button playButton;
+    public Button settingButton;
+    public Button exitButton;
+
     private void Start()
     {
         SlideInButtons();
         canvasGroup.DOFade(1f, duration);
+
+        playButton.onClick.AddListener(PlayButtonAnimation);
+        settingButton.onClick.AddListener(SettingButtonAnimation);
+        exitButton.onClick.AddListener(ExitButtonAnimation);
     }
 
     private void SlideInButtons()
@@ -31,5 +40,24 @@ public class UIController : MonoBehaviour
                 .SetEase(Ease.OutBack)
                 .SetDelay(i * delayBetweenButtons);
         }
+    }
+
+    public void PlayButtonAnimation()
+    {
+        playButton.transform.DOScale(1.2f, 0.2f)
+            .SetEase(Ease.OutBounce)
+            .OnComplete(() => playButton.transform.DOScale(1f, 0.2f));
+    }
+
+    public void SettingButtonAnimation()
+    {
+        settingButton.transform.DORotate(new Vector3(0, 0, 30), 0.2f, RotateMode.Fast)
+            .SetLoops(2, LoopType.Yoyo)
+            .SetEase(Ease.InOutBounce);
+    }
+
+    public void ExitButtonAnimation()
+    {
+        exitButton.transform.DOPunchScale(Vector3.one * 0.3f, 0.3f, 10, 1f);
     }
 }
